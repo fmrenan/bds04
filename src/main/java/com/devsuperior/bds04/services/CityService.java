da6a5681097b335556300f900b3102494c80c593
Renan Muniz <fmrenan@outlook.com>
2021-06-06 20:14:59 -0300

    City controller, repository, service

---
+++ src/main/java/com/devsuperior/bds04/services/CityService.java
@@ -0,0 +1,41 @@
+package com.devsuperior.bds04.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bds04.dto.CityDTO;
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.repository.CityRepository;
+import com.devsuperior.bds04.services.exceptions.DataBaseException;
+import com.devsuperior.bds04.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class CityService {
+
+	@Autowired
+	private CityRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<CityDTO> findAll(){
+		List<City> list = repository.findAll(Sort.by("name"));
+		
+		return list.stream().map(ct -> new CityDTO(ct)).collect(Collectors.toList());
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found: " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataBaseException("Integrity violation");
+		}
+	}
+}
